0024667635ccc38c00225ed27a6468c840898780
nate-moo <40650681+nate-moo@users.noreply.github.com>
2022-03-12 00:27:24 -0500

    large refactor to make it harder/easier to read

--- CalcPoly.py
+++ CalcPoly.py
@@ -3,49 +3,63 @@ from time import time
 
 t = Turtle()
 s = Screen()
-
-### Window Props
-s.setup(500, 500) # Window Size
-s.xscale = 15     # x axis scale
-s.yscale = 0.2    # y axis scale
-iyscale = 6
-xs = -10          # X axis start
-xe = 10           # X axis end
-resolution = 0.25
-t.speed(0)        # Speed of pen
-
-print(s.canvheight)
-print(s.canvwidth)
-
-t.goto(xe, 0)
-t.goto(xs, 0)
-t.goto(0, 0)
-t.goto(0, xs*10)
-t.goto(0, xe*10)
-t.goto(0, 0)
-
-t.penup()
-y = lambda x: 2*x**3 + (5*x)**2 + x
-t.goto(xs, 0)
 computed = list()
-st = time()
-while xs < xe:
-    computed.append([xs, y(xs)])
-    print(xs, computed)
-    xs+=resolution
 
-e = time()
+### Window Props ###
+s.setup(500, 500) # Window Size
+s.xscale = 10    # x axis scale
+s.yscale = 1    # y axis scale
+iyscale = 6
+xs = -20          # X axis start
+xe = 20           # X axis end
+resolution = .1
+t.speed(0)        # Speed of pen
+y = lambda x: x**3 - x**2 - 14*x + 32
+### ------ ###
 
-std = time()
-for a, i in computed:
-    if abs(i) < s.canvheight * iyscale:
-        print("Drawn", True)
-        t.goto(a, i)
-        t.pendown()
-        t.dot(2)
-    else:
-        print("Drawn", False)
-ed = time()
+def _compute(xs):
+    st = time()
+    while xs < xe:
+        computed.append([xs, y(xs)])
+        # print([xs, y(xs)])
+        xs += resolution
+    e = time()
+    return e, st
+
+def _draw():
+    std = time()
+    for a, i in computed:
+        if abs(i) < s.canvheight:
+            # print("Drawn", True)
+            t.goto(a, i)
+            t.pendown()
+            t.dot(2)
+        # else:
+            # print("Drawn", False)
+    ed = time()
+    return ed, std
+
+def setup():
+    t.screen = s
+    t.goto(xe, 0)
+    t.write(f"{xe}")
+    t.goto(xs, 0)
+    t.write(f"{xs}")
+    t.goto(0, 0)
+    t.goto(0, xs*10)
+    t.write(f"{xs*10}")
+    t.goto(0, xe*10)
+    t.write(f"{xe*10}")
+    t.goto(0, 0)
+    t.penup()
+    t.goto(xs, 0)
+
+    return _compute(xs), _draw()
+
+(e, st), (ed, std) = setup()
+
 print("Calculation took", f"{round(e-st, 4)}s")
 print("drawing took:", f"{round(ed - std, 4)}s")
+# print(t.screen.window_height())
+print()
 input()
